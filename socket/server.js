0c91fc7d45eb781ae1b77309057304be8513296c
idada01 <122584457+idada01@users.noreply.github.com>
2023-05-07 21:47:39 +0900

    websocket
    first commit

---
+++ socket/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use("/", function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080);
+
+const WebSocket = require('ws');
+
+const socket = new WebSocket.Server({
+    port: 8081
+});
+
+socket.on('connection', (ws, req)=>{
+
+    ws.on('message', (msg)=>{
+        console.log(`유저가 보낸거 : ${ msg}`);         // The message 'ㅎㅇ' is displayed on the server
+        ws.send('꺼져');        // the server responds
+    })
+
+})
+
